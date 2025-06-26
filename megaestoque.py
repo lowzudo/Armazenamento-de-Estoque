1a042f6dd2549ce9491443c6db9a86c72d035556
lowzudo <josiasgostosao12345@gmail.com>
2025-06-25 23:22:52 -0300

    Final Commit

--- megaestoque.py
+++ megaestoque.py
@@ -12,31 +12,26 @@ class Megaestoque:
         self.mercado = MercadoEstoque()
         self.cosmeticos = Cosmeticos("", 0, 0)
         self.estoque = {
-            "computadores": PcGamer.estoque,
-            "mercado": MercadoEstoque.estoque,
-            "cosmeticos": Cosmeticos.estoque
+            "computadores": self.pc_gamer.estoque,
+            "mercado": self.mercado.estoque,
+            "cosmeticos": self.cosmeticos.estoque
         }
 
     def iniciar(self):
-
-        print("Bem-vindo ao Mega Estoque!")
-        print("")
-        print("Você está prestes a criar uma conta.")
-        print("")
-        print("Por favor, siga as instruções para criar sua conta.")
-        print("")
+        print("Bem-vindo ao Mega Estoque!\n")
+        print("Você está prestes a criar uma conta.\n")
+        print("Por favor, siga as instruções para criar sua conta.\n")
         self.usuario.criar_conta()
 
         while True:
-
             print("--MENU--")
             print("1. Computadores")
             print("2. Mercado")
             print("3. Cosméticos")
             print("4. Sair")
 
-            pergunta = validacao_de_input("Esolha uma opção: ", int, "Opção inválida.")
-            match pergunta:
+            opcao = validacao_de_input("Escolha uma opção: ", int, "Opção inválida.")
+            match opcao:
                 case 1:
                     self.pc_gamer.menu()
                 case 2:
@@ -44,16 +39,11 @@ class Megaestoque:
                 case 3:
                     self.cosmeticos.menu()
                 case 4:
-                    print("Saindo do Mega Estoque.")
-                    print("")
+                    print("Saindo do Mega Estoque.\n")
                     print(f"Todo o estoque: {self.estoque}")
                     return
                 case _:
                     print("Opção inválida. Tente novamente.")
-                
-
-
-
 
 
 megaestoque1 = Megaestoque()
